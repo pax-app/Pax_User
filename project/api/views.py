fac22408a9c36717bf099ac94b313d493c58fb97
Gabriel Albino <enggabrielalbino@gmail.com>
2019-09-27 01:22:37 -0300

    fixing useless import and wrong function name

--- project/api/views.py
+++ project/api/views.py
@@ -1,53 +1,45 @@
 from flask import request, jsonify, Blueprint
-from flask_restful import Resource, reqparse
 
-parser = reqparse.RequestParser()
-parser.add_argument(
-    'username', help='This field cannot be blank', required=True)
-parser.add_argument(
-    'password', help='This field cannot be blank', required=True)
 
 users_blueprint = Blueprint('user', __name__)
 
 # User Registration Route
-@users_blueprint.route('/users/registration', methods=['POST'])
+@users_blueprint.route('/auth/registration', methods=['POST'])
 def user_registration():
-    data = parser.parse_args()
     return jsonify({
-        data
+        'message': 'registration'
     })
 
 # User Login Route
-@users_blueprint.route('/users/login', methods=['POST'])
+@users_blueprint.route('/auth/login', methods=['POST'])
 def user_login():
-    data = parser.parse_args()
     return jsonify({
-        data
+        'message': 'login'
     })
 
 # Logout for access
-@users_blueprint.route('/users/logout/access', methods=['POST'])
+@users_blueprint.route('/auth/logout/access', methods=['POST'])
 def user_logout_access():
     return jsonify({
         'message': 'logoutAccess'
     })
 
 # Logout for Refresh Token
-@users_blueprint.route('/users/logout/refresh', methods=['POST'])
+@users_blueprint.route('/auth/logout/refresh', methods=['POST'])
 def user_logout_refresh():
     return jsonify({
         'message': 'logoutRefresh'
     })
 
 # Token Refresh
-@users_blueprint.route('/users/token/refresh', methods=['POST'])
+@users_blueprint.route('/auth/token/refresh', methods=['POST'])
 def user_token_refresh():
     return jsonify({
         'message': 'tokenRefresh'
     })
 
 # Test Propourses (will be removed)
-@users_blueprint.route('/users/all', methods=['GET', 'DELETE'])
+@users_blueprint.route('/auth/all', methods=['GET', 'DELETE'])
 def all_users():
     if request.method == 'GET':
         return jsonify({
@@ -60,7 +52,7 @@ def all_users():
 
 # secret endpoint (to test access with tokens)
 @users_blueprint.route('/secret-resource', methods=['GET'])
-def all_users():
+def secret_resource():
     return jsonify({
         'answer': 42
     })
