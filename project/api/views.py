ab69a014d0c9900bd2f6d21606bf06c934f83490
Gabriel Albino <enggabrielalbino@gmail.com>
2019-09-27 00:38:30 -0300

    Setting initial routes

--- project/api/views.py
+++ project/api/views.py
@@ -1,11 +1,66 @@
 from flask import request, jsonify, Blueprint
+from flask_restful import Resource, reqparse
 
-user_blueprint = Blueprint('user', __name__)
+parser = reqparse.RequestParser()
+parser.add_argument(
+    'username', help='This field cannot be blank', required=True)
+parser.add_argument(
+    'password', help='This field cannot be blank', required=True)
 
+users_blueprint = Blueprint('user', __name__)
 
-@users_blueprint.route('/users/ping', methods=['GET'])
-def ping_pong():
+# User Registration Route
+@users_blueprint.route('/users/registration', methods=['POST'])
+def user_registration():
+    data = parser.parse_args()
     return jsonify({
-        'status': 'success',
-        'message': 'pong!'
+        data
+    })
+
+# User Login Route
+@users_blueprint.route('/users/login', methods=['POST'])
+def user_login():
+    data = parser.parse_args()
+    return jsonify({
+        data
+    })
+
+# Logout for access
+@users_blueprint.route('/users/logout/access', methods=['POST'])
+def user_logout_access():
+    return jsonify({
+        'message': 'logoutAccess'
+    })
+
+# Logout for Refresh Token
+@users_blueprint.route('/users/logout/refresh', methods=['POST'])
+def user_logout_refresh():
+    return jsonify({
+        'message': 'logoutRefresh'
+    })
+
+# Token Refresh
+@users_blueprint.route('/users/token/refresh', methods=['POST'])
+def user_token_refresh():
+    return jsonify({
+        'message': 'tokenRefresh'
+    })
+
+# Test Propourses (will be removed)
+@users_blueprint.route('/users/all', methods=['GET', 'DELETE'])
+def all_users():
+    if request.method == 'GET':
+        return jsonify({
+            'message': 'allUsersGet'
+        })
+    else:
+        return jsonify({
+            'message': 'allUsersDelete'
+        })
+
+# secret endpoint (to test access with tokens)
+@users_blueprint.route('/secret-resource', methods=['GET'])
+def all_users():
+    return jsonify({
+        'answer': 42
     })
